410b721c3e9c2b61f4b387f879ab713ac160eea6
Andy Luu <andyluusj@yahoo.com>
2019-05-15 23:26:11 -0700

    home late 05/15/19

--- PyApps/PDExample.py
+++ PyApps/PDExample.py
@@ -104,3 +104,12 @@ var1 = ds.var1.str.split(',', expand=True).values.ravel() #similar to .flatten()
 var2 = np.repeat(ds.var2.values, len(var1)/len(ds))
 ds1 = pd.DataFrame({'var1': var1, 'var2': var2})
 print(ds1)
+
+'''General unnesting function'''
+def unnesting(df, explode):
+    idx = df.index.repeat(df[explode[0]].str.len())
+    df1 = pd.concat([
+        pd.DataFrame({x: np.concatenate(df[x].values)}) for x in explode], axis=1)
+    df1.index = idx
+    return df1.join(df.drop(explode, 1), how='left')
+
